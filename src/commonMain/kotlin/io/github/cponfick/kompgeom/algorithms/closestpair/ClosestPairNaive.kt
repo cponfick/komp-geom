9f5679cc2ba326469773b2b84a962a363397c6dd
cponfick <95411546+cponfick@users.noreply.github.com>
2025-07-19 17:12:06 +0200

    refactor: update ClosestPairNaive and IClosestPair to use Vector interface

--- src/commonMain/kotlin/io/github/cponfick/kompgeom/algorithms/closestpair/ClosestPairNaive.kt
+++ src/commonMain/kotlin/io/github/cponfick/kompgeom/algorithms/closestpair/ClosestPairNaive.kt
@@ -1,38 +1,38 @@
-// package io.github.cponfick.kompgeom.algorithms.closestpair
-//
-// import io.github.cponfick.kompgeom.euclidean.EuclideanVector
-//
-/// **
-// * Closest Pair algorithm using the Divide and Conquer approach. This implementation sorts the
-// * points by x and y coordinates and recursively finds the closest pair.
-// *
-// * @property input collection of points in 2D space.
-// * @constructor initializes the algorithm with a collection of points.
-// */
-// public class ClosestPairNaive<V : EuclideanVector<V>>(input: Collection<V>) : IClosestPair<V> {
-//  private val points = input.toList()
-//
-//  init {
-//    if (input.size < 2) {
-//      throw IllegalArgumentException("Input must contain at least 2 elements")
-//    }
-//  }
-//
-//  override fun run(): Result<V> {
-//    var closestPair = Pair(points[0], points[1])
-//    var closestDistance = Double.POSITIVE_INFINITY
-//
-//    for (i in points.indices) {
-//      for (j in i + 1 until points.size) {
-//        val distance = points[i] distance points[j]
-//        if (distance < closestDistance) {
-//          closestDistance = distance
-//          closestPair = Pair(points[i], points[j])
-//        }
-//      }
-//    }
-//    return Result(closestDistance, closestPair)
-//  }
-//
-//  override fun getName(): String = "Closest Pair Naive"
-// }
+package io.github.cponfick.kompgeom.algorithms.closestpair
+
+import io.github.cponfick.kompgeom.core.Vector
+
+/**
+ * Closest Pair algorithm using the Divide and Conquer approach. This implementation sorts the
+ * points by x and y coordinates and recursively finds the closest pair.
+ *
+ * @property input collection of points in 2D space.
+ * @constructor initializes the algorithm with a collection of points.
+ */
+public class ClosestPairNaive<V : Vector<V>>(input: Collection<V>) : IClosestPair<V> {
+  private val points = input.toList()
+
+  init {
+    if (input.size < 2) {
+      throw IllegalArgumentException("Input must contain at least 2 elements")
+    }
+  }
+
+  override fun run(): Result<V> {
+    var closestPair = Pair(points[0], points[1])
+    var closestDistance = Double.POSITIVE_INFINITY
+
+    for (i in points.indices) {
+      for (j in i + 1 until points.size) {
+        val distance = points[i] distance points[j]
+        if (distance < closestDistance) {
+          closestDistance = distance
+          closestPair = Pair(points[i], points[j])
+        }
+      }
+    }
+    return Result(closestDistance, closestPair)
+  }
+
+  override fun getName(): String = "Closest Pair Naive"
+}
